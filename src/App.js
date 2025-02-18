19c0ab3f6220cf556e775cd1a7840a176516895a
Elsheshtawey1 <mohamedelsheshtawey1@gmail.com>
2025-02-19 00:07:08 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import NavBar from "./NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      {loading ? (
+        <div className="loader">
+          {" "}
+          <ScaleLoader
+            color={"#f87765"}
+            loading={loading}
+            height={100}
+            margin={10}
+          />
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </>
+      )}
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+
+
+function NavBar() {
+  return (
+      <Navbar bg="dark" expand="lg">
+                <Container>
+                    <NavLink to="/"><img src="images/618b5b33c589e179c2c8b27c_Shiloh New Logo-01-p-3200.png" alt="" /></NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="NavLinks justify-content-end">
+                            <HashLink to="/#latest">Latest Builds</HashLink>
+                            <HashLink to="/#process">Process</HashLink>
+                            <HashLink to="/#service">Services</HashLink>
+                            <HashLink to="/#contact">Contact Us</HashLink>
+                            <NavLink to="/Projects">Projects</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar >
+  );
+}
+
+
+export default NavBar;
